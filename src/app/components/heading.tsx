1f9faeabe9eae9ba7a070c636569a5d7c37b84b0
orzarhi <or.zarhi11@gmail.com>
2024-10-31 23:08:50 +0200

    chore: Add new components and update dependencies

---
+++ src/app/components/heading.tsx
@@ -0,0 +1,21 @@
+import { cn } from '@/utils';
+import { HTMLAttributes, ReactNode } from 'react';
+
+interface HeadingProps extends HTMLAttributes<HTMLHeadingElement> {
+  className?: string;
+  children: ReactNode;
+}
+
+export const Heading = ({ className, children, ...props }: HeadingProps) => {
+  return (
+    <h1
+      className={cn(
+        'text-4xl sm:text-5xl text-pretty font-heading font-semibold tracking-tight text-zinc-800',
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </h1>
+  );
+};
---
+++ src/app/components/index.ts
@@ -0,0 +1,3 @@
+export { ShinyButton } from './shiny-button';
+export { Heading } from './heading';
+export { MaxWidthWrapper } from './max-width-wrapper';
